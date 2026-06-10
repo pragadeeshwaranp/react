382c0fcd096083c9da60c1cf4b65a5767346cc98
pragadeeshwaran p <prempragadeesh7@gmail.com>
2026-06-10 23:05:32 +0530

    Add CourseCard3 component for Data Science course

---
+++ CourseCard3.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./CourseCard.css";
+
+function CourseCard3() {
+  return (
+    <div className="course-card">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwzdlSia6znfGH5eP5WU_Dpb4MbNza5C3aYQ&s" alt="course" />
+      <h2>Data Science Basics</h2>
+      <p>Instructor: Michael Lee</p>
+      <p>Duration: 14 Weeks</p>
+      <p>Rating: ⭐ 4.9</p>
+      <p>Fee: $129</p>
+    </div>
+  );
+}
+
+export default CourseCard3;
